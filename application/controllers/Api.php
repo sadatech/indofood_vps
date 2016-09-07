f90a5e886cc59a49c73234a061749559455eb619
Sada <sadatech@github.com>
2016-09-07 15:46:57 +0700

    a

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2081,14 +2081,14 @@ $message = '
 ';
 
 // // To send HTML mail, the Content-type header must be set
-// $headers = 'From: rizaldi rizaldi354313@gmail.com' . "\r\n" ;
+$headers = 'From: rizaldi rizaldi354313@gmail.com' . "\r\n" ;
 //     $headers .='Reply-To: '. $to . "\r\n" ;
-//     $headers .='X-Mailer: PHP/' . phpversion();
-//     $headers .= "MIME-Version: 1.0\r\n";
-//     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";   
+    $headers .='X-Mailer: PHP/' . phpversion();
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";   
 
 // Mail it
-    mail($to, $subject, $message);
+    mail($to, $subject, $message, $headers);
   }
 
     // Report Related API
