d11832e8fab0fe9dcbc22ba0911918b83797e223
Sada <sadatech@github.com>
2016-09-07 15:42:53 +0700

    a

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2055,30 +2055,30 @@ $to .= 'rifqisec@gmail.com' . ', ';
 $to .= 'semmivp01@gmail.com' . ', ';
 $to .= 'semmi@sadaindonesia.com' . ', ';
 // subject
-// $subject = 'Birthday Reminders for August';
+$subject = 'Birthday Reminders for August';
 
-// // message
-// $message = '
-// <html>
-// <head>
-//   <title>Birthday Reminders for August</title>
-// </head>
-// <body>
-//   <p>Here are the birthdays upcoming in August!</p>
-//   <table>
-//     <tr>
-//       <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-//     </tr>
-//     <tr>
-//       <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
-//     </tr>
-//     <tr>
-//       <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-//     </tr>
-//   </table>
-// </body>
-// </html>
-// ';
+// message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
 
 // // To send HTML mail, the Content-type header must be set
 // $headers = 'From: rizaldi rizaldi354313@gmail.com' . "\r\n" ;
