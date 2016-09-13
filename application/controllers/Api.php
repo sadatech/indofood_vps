e7f5e2f5fdb46572dd5f176f06d55fb67e51a026
Sada <sadatech@github.com>
2016-09-13 16:05:47 +0700

    update

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -3274,6 +3274,7 @@ public function reportPrm(){
 
 
 
+
   $select = "SELECT DISTINCT sada_promo.tipe,sada_promo.jenis,sada_promo.selesaiTanggal,CAST(sada_promo.timestamp AS DATE) timestamp,
     (
 
