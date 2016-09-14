b48e2b79180137cff145046632a71ce7ddc88047
Sada <sadatech@github.com>
2016-09-14 15:15:54 +0700

    update

--- assets/custom/CountTotal.js
+++ assets/custom/CountTotal.js
@@ -24,101 +24,108 @@ $("#a").validate({
                 datas += "";
                 for (var i = data.length - 1; i >= 0; i--) {
                     // datas += "<tr><td>No</td>";
-                    datas += "<tr>";
-                    datas += "<td>"+data[i]['nama_cabang']+"</td>";
-                    datas += "<td>"+data[i]['nama_user']+"</td>";
-                    if (data[i]['stay_user'] == "Y") {
-                        datas += "<td>Stay</td>";
+                    if (data[i]['status'] == false) {
+                        datas += "<tr>";
+                        datas += "<td colspan='18'>Data Kosong</td>";
+                        datas += "</tr>";
                     }
-                    if (data[i]['stay_user'] == "N") {
-                        datas += "<td>Mobile</td>";
-                    };
-                    datas += "<td>"+data[i]['store_id']+"</td>";
-                    datas += "<td>"+data[i]['nama_toko']+"</td>";
+                        else{
+                        datas += "<tr>";
+                        datas += "<td>"+data[i]['nama_cabang']+"</td>";
+                        datas += "<td>"+data[i]['nama_user']+"</td>";
+                        if (data[i]['stay_user'] == "Y") {
+                            datas += "<td>Stay</td>";
+                        }
+                        if (data[i]['stay_user'] == "N") {
+                            datas += "<td>Mobile</td>";
+                        };
+                        datas += "<td>"+data[i]['store_id']+"</td>";
+                        datas += "<td>"+data[i]['nama_toko']+"</td>";
 
-                    datas += "<td>"+data[i]['contact_count']+"</td>";
-                    datas += "<td>"+data[i]['count_switching']+"</td>";
-                    datas += "<td>"+data[i]['count_recruit']+"</td>";
-                    
-				    // datas += "<td>"+data[i]['count_sampling']+"</td>";
-                    if (data[i]['BC'] == null) {
-                        datas += "<td>0</td>";
+                        datas += "<td>"+data[i]['contact_count']+"</td>";
+                        datas += "<td>"+data[i]['count_switching']+"</td>";
+                        datas += "<td>"+data[i]['count_recruit']+"</td>";
+                        
+    				    // datas += "<td>"+data[i]['count_sampling']+"</td>";
+                        if (data[i]['BC'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['BC']+"</td>";
+                        }
+                        if (data[i]['BTI'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['BTI']+"</td>";
+                        }
+                        if (data[i]['Rusk'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['Rusk']+"</td>";
+                        }
+                        if (data[i]['Pudding'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['Pudding']+"</td>";
+                        }
+                        if (data[i]['Others'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['Others']+"</td>";
+                        }
+                        // if (data[i]['strike_sampling'] == null) {
+                        //     datas += "<td>0</td>";
+                        // }
+                        // else{
+                        //     datas += "<td> "+data[i]['strike_sampling']+"</td>";
+                        // }
+                        if (data[i]['strike_sampling_bc'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['strike_sampling_bc']+"</td>";
+                        }
+                        if (data[i]['strike_sampling_bti'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['strike_sampling_bti']+"</td>";
+                        }
+                        if (data[i]['strike_sampling_rusk'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['strike_sampling_rusk']+"</td>";
+                        }
+                        if (data[i]['strike_sampling_pudding'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['strike_sampling_pudding']+"</td>";
+                        }
+                        if (data[i]['strike_sampling_others'] == null) {
+                            datas += "<td>0</td>";
+                        }
+                        else{
+                            datas += "<td> "+data[i]['strike_sampling_others']+"</td>";
+                        }
+                        // datas += "<td>"+data[i]['tipe']+"</td>";
+                        // datas += "<td>"+data[i]['BTI']+"</td>";
+                        // datas += "<td>"+data[i]['Rusk']+"</td>";
+                        // // datas += "<td>"+data[i]['tipe']+"</td>";
+                        // datas += "<td>"+data[i]['Pudding']+"</td>";
+                        // datas += "<td>"+data[i]['Others']+"</td>";
+                        
+                        // datas += "<td>"+data[i]['strike_sampling']+"</td>";
+                        // datas += "<td>"+data[i]['sampling']+"</td>";
+                        // datas += "<td>"+data[i]['segmen']+"</td>";
+                        // datas += "<td>"+data[i]['sada_kategori_label']+"</td></tr>";
+                        aaa += "<td>"+data[i]['sada_kategori_label']+"</td>";
                     }
-                    else{
-                        datas += "<td> "+data[i]['BC']+"</td>";
-                    }
-                    if (data[i]['BTI'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['BTI']+"</td>";
-                    }
-                    if (data[i]['Rusk'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['Rusk']+"</td>";
-                    }
-                    if (data[i]['Pudding'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['Pudding']+"</td>";
-                    }
-                    if (data[i]['Others'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['Others']+"</td>";
-                    }
-                    // if (data[i]['strike_sampling'] == null) {
-                    //     datas += "<td>0</td>";
-                    // }
-                    // else{
-                    //     datas += "<td> "+data[i]['strike_sampling']+"</td>";
-                    // }
-                    if (data[i]['strike_sampling_bc'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['strike_sampling_bc']+"</td>";
-                    }
-                    if (data[i]['strike_sampling_bti'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['strike_sampling_bti']+"</td>";
-                    }
-                    if (data[i]['strike_sampling_rusk'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['strike_sampling_rusk']+"</td>";
-                    }
-                    if (data[i]['strike_sampling_pudding'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['strike_sampling_pudding']+"</td>";
-                    }
-                    if (data[i]['strike_sampling_others'] == null) {
-                        datas += "<td>0</td>";
-                    }
-                    else{
-                        datas += "<td> "+data[i]['strike_sampling_others']+"</td>";
-                    }
-                    // datas += "<td>"+data[i]['tipe']+"</td>";
-                    // datas += "<td>"+data[i]['BTI']+"</td>";
-                    // datas += "<td>"+data[i]['Rusk']+"</td>";
-                    // // datas += "<td>"+data[i]['tipe']+"</td>";
-                    // datas += "<td>"+data[i]['Pudding']+"</td>";
-                    // datas += "<td>"+data[i]['Others']+"</td>";
-                    
-                    // datas += "<td>"+data[i]['strike_sampling']+"</td>";
-                    // datas += "<td>"+data[i]['sampling']+"</td>";
-                    // datas += "<td>"+data[i]['segmen']+"</td>";
-                    // datas += "<td>"+data[i]['sada_kategori_label']+"</td></tr>";
-                    aaa += "<td>"+data[i]['sada_kategori_label']+"</td>";
                 }
                 $("#excelCDetail").attr('href',url);
                 $("#dataContactTotal").html(datas);    
