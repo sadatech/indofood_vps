a20abbc6b660c49c9a7543bc10efce1275981aab
Sada <sadatech@github.com>
2016-09-14 17:05:19 +0700

    update

--- assets/custom/kategori_segmen.js
+++ assets/custom/kategori_segmen.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
     // $(this).attr("id").inputmask('Rp 999.999.999', { numericInput: true });
     var message_status = $("#status");
     var base_url = window.location.origin;
+    $("td[contenteditable=true]").inputmask('Rp 999.999.999', { numericInput: true });
     $("td[contenteditable=true]").blur(function(){
         var id_kat = $(this).attr("id");
         var value = $(this).text();
-        $(this).attr("id").inputmask('Rp 999.999.999', { numericInput: true });
+
         $.post(base_url+'/kategori_segmen.jsp' , id_kat + "=" + value, function(data){
             if(data != '')
             {
