bacb3683233eeb77baf7a29451474d48aea74469
Sada <sadatech@github.com>
2016-09-14 09:10:11 +0700

    update

--- application/views/page/login.php
+++ application/views/page/login.php
@@ -51,7 +51,7 @@
                 </div>
                 <div class="col-md-6 login-container bs-reset">
                     <div class="login-content">
-                        <h1>Retail Apps BA Pro</h1>
+                        <h1>Retail Application BA Promina</h1>
                         <!-- <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
