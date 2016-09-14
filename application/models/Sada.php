f0ab8d8c5a4715df4c2f56ce7432dbdb5e7dd1ac
Sada <sadatech@github.com>
2016-09-14 11:07:25 +0700

    update

--- application/models/Sada.php
+++ application/models/Sada.php
@@ -67,6 +67,8 @@ class Sada extends CI_Model{
         $response[$value->id_produk]['monthVolume']= $value->monthVolume;
 
         $response[$value->id_produk]['prices']= $value->monthVolume * $value->price;
+
+        $response[$value->id_produk]['price']= $value->price;
       }
       if (isset($value->monthAgoVolume)) {
         $response[$value->id_produk]['monthAgoVolume']= $value->monthAgoVolume;
