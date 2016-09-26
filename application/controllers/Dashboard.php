6a37dcffef76e43a43afad039d286233973104e3
Sada <sadatech@github.com>
2016-09-26 11:34:12 +0700

    upd

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1448,7 +1448,7 @@ public function EditdataUser()
 		$qry = $this->db->select('id_user,id_toko')->where('id_user',$dataDas['paramId'])->get('sada_tl_in_kota');
 		$toko_id = array();
 		foreach ($qry->result() as $get_toko) {
-			$toko_id []= $get_toko->id_toko;
+			$toko_id[] = $get_toko->id_toko;
 		}
 		print_r($toko_id);
 		$qry2 = $this->db->select('id_cabang,id_kota,nama_kota')->where('id_kota',$qry->id_toko)->get('sada_kota')->row();
