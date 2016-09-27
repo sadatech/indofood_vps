742d96fbf1cc7516341bfbd9dd16fde79447a125
Sada <sadatech@github.com>
2016-09-27 12:17:44 +0700

    upd

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1282,7 +1282,7 @@ public function AdddataUser()
 
 
 
-			$dataDas['js'][]	= "assets/custom/s.js";
+			$dataDas['js'][]	= "assets/custom/addUser.js";
 
 
 
@@ -1392,7 +1392,7 @@ public function AdddataUser()
 
 		$dataDas['query_toko'] = $this->db->get_where('sada_toko',array('status'=>"Y"))->result();
 
-		$dataDas['js'][]	= "assets/custom/addUser.js";
+		$dataDas['js'][]	= "assets/custom/editUser.js";
 
 		$this->load->view('view_awal', $dataDas, FALSE);
 
