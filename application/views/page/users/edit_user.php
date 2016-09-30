a6998a21ad5fd00a93bd1c52cf2097f1428a348a
Sada <sadatech@github.com>
2016-09-30 15:52:34 +0700

    upd

--- application/views/page/users/edit_user.php
+++ application/views/page/users/edit_user.php
@@ -338,12 +338,13 @@
 
                                                         <?php
                                                             foreach ($tokoa->result() as $tokos) {
+                                                                print_r(array($tokos->id_toko));
                                                                     $data = $this->db->select('nama,id_toko')->where('id_toko',$tokos->id_toko)->get('sada_toko');
 
                                                                     foreach ($data->result() as $l) {
                                                                         echo "<option selected value='".$l->id_toko."'>".$l->nama."</option>";
                                                                     }
-                                                                    
+
 
                                                             }
 
