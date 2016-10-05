367ff71e5e3b90ed0fc283e7a98090b282e51bb3
Sada <sadatech@github.com>
2016-10-05 14:55:53 +0700

    upd

--- application/models/Sada.php
+++ application/models/Sada.php
@@ -516,7 +516,7 @@ WHERE
   AND '$endDate'
   ORDER BY id_toko
   ";
-  echo $volume;
+  echo $sql_volume;
   $volume = $this->db->query($sql_volume)->result();
   // $volume = $this->db->select(['id_toko ', 'SUM(qty) monthVolume'])
   // ->from('sada_produk_terjual')
