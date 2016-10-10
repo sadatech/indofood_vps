158bbf898a465d2a51dcc6dd0505c404831b3e3a
Sada <sadatech@github.com>
2016-10-10 10:45:23 +0700

    update login

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1556,17 +1556,17 @@ public function UpdateEditUser()
 						foreach ($id_tokos as $toko_id) {
 								$updateTLs['id_user'] = $this->input->post("id_us",TRUE);
 								$updateTLs['id_toko'] = $toko_id;
-								// $this->db->insert("sada_tl_in_kota",$updateTLs);
-								print_r($updateTLs);
+								$this->db->insert("sada_tl_in_kota",$updateTLs);
+								// print_r($updateTLs);
 								// echo $toko_id;
 						
 						}
-						// print_r($id_tokos);
+						print_r($id_tokos);
 				}
 
-					$this->session->set_flashdata('msg', 'User Success Updated');
+					// $this->session->set_flashdata('msg', 'User Success Updated');
 
-					redirect('Dashboard/dataUser', 'refresh');
+					// redirect('Dashboard/dataUser', 'refresh');
 			}
 
 			elseif ($dataUpdate['akses'] == 1) {
