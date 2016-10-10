023c905c91dd10ff9628b4d875493c7ddbfb784d
Sada <sadatech@github.com>
2016-10-10 15:38:43 +0700

    update login

--- application/libraries/Excel.php
+++ application/libraries/Excel.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+require_once APPPATH."/libraries/Carbon.php";
 require_once APPPATH."/third_party/PHPExcel.php";
 require_once APPPATH.'/third_party/PHPExcel/Cell/AdvancedValueBinder.php';
 
@@ -8,7 +9,6 @@ class Excel extends PHPExcel {
   private $excel;
     public function __construct() {
         parent::__construct();
-        $this->load->library("Carbon");
         PHPExcel_Cell::setValueBinder( new PHPExcel_Cell_AdvancedValueBinder() );
     }
     public function downloadReportOutOfStock($data,$val)
