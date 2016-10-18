c1ae35a4b6d0dea3e6b18a23677b2542a5f1302e
Sada <sadatech@github.com>
2016-10-18 11:49:39 +0700

    contact total

--- application/views/page/contact/contact_total.php
+++ application/views/page/contact/contact_total.php
@@ -1,64 +1,99 @@
-<style type="text/css"></style>
-<form action="#" id="a" class="form-horizontal" method="post">
-    <div class="table-toolbar">
-        <div class="row">
-            <div class="col-xs-12">
+<style type="text/css">
 
-                <div class="mt-element-ribbon" style="background: white;">
+</style>
 
-                    <div class="ribbon ribbon-border-hor ribbon-clip ribbon-color-danger uppercase">
+<div class="row">
 
-                        <div class="ribbon-sub ribbon-clip"></div>Data <?php echo $title; ?>
+
+
+    <div class="col-xs-12">
+
+        <div class="mt-element-ribbon" style="background: white;">
+
+            <div class=
+
+            "ribbon ribbon-border-hor ribbon-clip ribbon-color-danger uppercase">
+
+            <div class="ribbon-sub ribbon-clip"></div>Data <?php echo $title; ?>
+
+
+
+        </div>
+
+    </div>
+
+</div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+
+
+
+        <!-- BEGIN EXAMPLE TABLE PORTLET-->
+
+        <div class="portlet light bordered">
+
+            <div class="portlet-title">
+
+                <div class="caption font-dark">
+
+                    <i class="icon-settings font-dark"></i>
+
+                    <span class="caption-subject bold uppercase"> Data <?php echo $title; ?></span>
+
+                </div>
+
+
+
+            </div>
+
+            <form action="#" id="a" class="form-horizontal" method="post">
+
+                <div class="form-group">
+
+                    <div class="col-md-12">
+
+                        <!-- <select id="tl" name="tl"  class=" form-control select2"  data-width="18%"></select> -->
+
+                        <select id="ba" name="ba" class="  form-control select2" data-width="18%"></select>
+
+                        <select id="toko" name="toko"  class=" form-control select2"  data-width="18%"></select>
+
+                        <select id="cabang" name="cabang"  class="select2 form-control"  data-width="18%"></select>
+
+                        <select id="kota" name="kota"  class="select2 form-control"  data-width="18%"></select>
+
+                    </div>
+
+                    <div class="col-md-8" style="margin-top:15px;margin-left:-15px">
+
+                        <div class="col-md-3">
+                        <input id="startDate"type="text" class="bs-select form-control input-small date-picker" value="05/29/2016" name="startDate" placeholder="Start Date" data-width="13%">
+
+                    </div>
+                    <div class="col-md-3">
+                        <input id="endDate" type="text" class="bs-select form-control input-small date-picker" name="endDate" value="<?php echo date("m/d/Y"); ?>" placeholder="End Date" data-width="13%">
+                    </div>
+
+                        <div class="col-md-2">
+
+                            <button type="submit" class="btn sbold green">Filter</button>
+
+                        </div>
 
                     </div>
 
                 </div>
 
-            </div>
+            </form>
         </div>
-
-
-        <div class="row">
-            <div class="col-md-12">
-                <div class="portlet light bordered">
-
-                    <div class="portlet-title">
-
-                        <div class="caption font-dark">
-
-                            <i class="icon-settings font-dark"></i>
-
-                            <span class="caption-subject bold uppercase"> Data <?php echo $title; ?></span>
-
-                        </div>
-                        <div class="form-group">
-                          <div class="col-md-12">
-
-                            <select id="ba" name="ba" class="  form-control select2" data-width="18%"></select>
-                            <select id="toko" name="toko"  class=" form-control select2"  data-width="18%"></select>
-                            <select id="cabang" name="cabang"  class="select2 form-control"  data-width="18%"></select>
-                            <select id="kota" name="kota"  class="select2 form-control" data-width="18%"></select>
-                        </div>
-                        <div class="col-md-8" style="margin-top:15px;margin-left:-15px">
-                          <div class="col-md-3">
-                            <input type="text" class="bs-select form-control input-small date-picker" name="startDate" placeholder="Start Date" data-width="13%">
-
-                        </div>
-                        <div class="col-md-3">
-                            <input type="text" class="bs-select form-control input-small date-picker" name="endDate" placeholder="End Date" data-width="13%">
-                        </div>
-                        <div class="col-md-2">
-                          <button type="submit" class="btn sbold green">Filter</button>
-                      </div>
-                  </div>
-              </div>
-          </form>
-      </div>
-  </div>
+    </div>
 </div>
 
+
 <div class="row">
-    <div class="col-md-12">
+            <div class="col-md-12">
       <div class="portlet light bordered">
 
         <div class="table-toolbar">
@@ -67,31 +102,53 @@
 
                 <div class="col-md-6">
 
-                    
+
 
                 </div>
-                <div class="btn-group" style="float: right;">
 
-                    <a href="#" class="btn sbold green" id="excelCDetail">Download Excel</a>
-
-                </div>
             </div>
-            <table class="table table-striped table-bordered table-hover table-checkable order-column" id="dataContactTotal">
+
+            <div class="btn-group" style="float: right;">
+
+                <a href="#" class="btn sbold green" id="excelreportprm">Download Excel</a>
+
+            </div>
+
+        </div>
+    <table class="table table-striped table-bordered table-hover table-checkable order-column" id="dataContactTotal">
 
         <thead>
 
-            <tr>
+            <tr align='center'>
+                        <th>Cabang</th>
+                        <th>Nama BA</th>
+                        <!-- <throwspan="2">Nama TL</th> -->
+                        <th>Status ( Mobile / Stay )</th>
+                        <th>Costumer Id</th>
+                        <th>Nama Store</th>
+                        <th>Contact</th>
+                        <th>Sampling</th>
+                        <th>Strike Sampling</th>
+                    </tr>
+                   <!--  <tr id="sampling">
+                        <td>Contact</td>
+                        <td>Switching</td>
+                        <td>New Recruit</td>
 
-                <th>Cabang</th>
-                <th>Nama BA</th>
-                <!-- <th >Nama TL</th> -->
-                <th >Status ( Mobile / Stay )</th>
-                <th >Costumer Id</th>
-                <th >Nama Store</th>
-                <th>Contact</th>
-                <th>Sampling</th>
-                <th>Strike Sampling</th>
-
+                        <td>BC</td>
+                        <td>BTI</td>
+                        <td>Rusk </td>
+                        <td>Pudding</td>
+                        <td>Others</td>
+                        
+                        <td>BC</td>
+                        <td>BTI</td>
+                        <td>Rusk </td>
+                        <td>Pudding</td>
+                        <td>Others</td>
+                    </tr>
+                    <tr> -->
+                        
             </tr>
 
         </thead>
@@ -103,14 +160,10 @@
         </tbody>
 
     </table>
-            <!-- <div class="btn-group">
-                <a href="#" class="btn sbold green" id="excelCDetail">Download Excel</a>
-            </div> -->
-        </div>
+
 
     </div>
 </div>
 </div>
 
-</div>
 </div>
