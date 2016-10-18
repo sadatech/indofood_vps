c09d12d64a46b58ce0ebb4d308e7afc125797e31
Sada <sadatech@github.com>
2016-10-18 10:21:37 +0700

    contact total

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4173,9 +4173,7 @@ public function reporttotalcontact()
 		sada_form_contact.tgl_contact BETWEEN '".$arr['startDate']."'
 		AND '".$arr['endDate']."'";
 
-		if($arr['ba'] != ""){
-			$q .= " AND sada_form_contact.user_id = '".$arr['ba']."'";
-		}
+		$q .= ($arr['ba'] != "") ? " AND sada_form_contact.user_id = '".$arr['ba']."'" : "";
 
 		$q .="GROUP BY
 		date(
