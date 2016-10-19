5578b01b15237cadb4c9504cb74e66bd21a900f5
Sada <sadatech@github.com>
2016-10-19 09:39:42 +0700

    contact total

--- application/models/Sada.php
+++ application/models/Sada.php
@@ -2030,9 +2030,9 @@ public function contactTotal($arr = array(),$limit_excel)
     ),
     sada_form_contact.user_id,
     sada_form_contact.store_id";
-
+    echo $q;
     // $q .= ($limit_excel=="") ? " LIMIT 10" : "";
-    return $this->db->query($q);
+    // return $this->db->query($q);
 }
 
 /* End Query Promo */
