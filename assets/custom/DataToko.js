ff2f84bf11eb0b5bd9f62ddd3f8080cc971280a6
sadatech <sadatech@github.com>
2016-11-01 14:55:23 +0700

    upd target

--- assets/custom/DataToko.js
+++ assets/custom/DataToko.js
@@ -86,5 +86,5 @@ function loadPage(url)  //the function that loads pages via AJAX
 
 function updatetargettoko(target,id_target) {
     console.log(target);    
-    console.log(id_target);    
+    // console.log(id_target);    
 }//kampret
