2818534ab065bc2a438a12879416aca9c74d4dd8
sadatech <sadatech@github.com>
2016-11-08 18:36:25 +0700

    upd index

--- index.php
+++ index.php
@@ -54,12 +54,12 @@ date_default_timezone_set('Asia/Jakarta');
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-if ($_SERVER['SERVER_NAME'] == 'local_server_name') {
-    define('ENVIRONMENT', 'development');
-} else {
-    define('ENVIRONMENT', 'production');
-}
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+// if ($_SERVER['SERVER_NAME'] == 'local_server_name') {
+//     define('ENVIRONMENT', 'development');
+// } else {
+//     define('ENVIRONMENT', 'production');
+// }
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -69,22 +69,8 @@ if ($_SERVER['SERVER_NAME'] == 'local_server_name') {
  * By default development will show errors but testing and live will hide them.
  */
 if (defined('ENVIRONMENT')) {
-    // switch (ENVIRONMENT) {
-    //     case 'development':
-    //         error_reporting(E_ALL);
-    //         break;
-    //     case 'testing':
-    //     case 'production':
-    //         error_reporting(0);
-    //         ini_set('display_errors', 0);  
-    //         break;
-    //     default:
-    //         exit('The application environment is not set correctly.');
-    // }
-}
-switch (ENVIRONMENT)
-{
-	case 'development':
+    switch (ENVIRONMENT) {
+        case 'development':
             error_reporting(E_ALL);
             break;
         case 'testing':
@@ -94,28 +80,42 @@ switch (ENVIRONMENT)
             break;
         default:
             exit('The application environment is not set correctly.');
+    }
+}
+switch (ENVIRONMENT)
+{
 	// case 'development':
-	// 	error_reporting(-1);
-	// 	ini_set('display_errors', 1);
-	// break;
+ //            error_reporting(E_ALL);
+ //            break;
+ //        case 'testing':
+ //        case 'production':
+ //            error_reporting(0);
+ //            ini_set('display_errors', 0);  
+ //            break;
+ //        default:
+ //            exit('The application environment is not set correctly.');
+	case 'development':
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+	break;
 
-	// case 'testing':
-	// case 'production':
-	// 	ini_set('display_errors', 0);
-	// 	if (version_compare(PHP_VERSION, '5.3', '>='))
-	// 	{
-	// 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-	// 	}
-	// 	else
-	// 	{
-	// 		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-	// 	}
-	// break;
+	case 'testing':
+	case 'production':
+		ini_set('display_errors', 0);
+		if (version_compare(PHP_VERSION, '5.3', '>='))
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		}
+		else
+		{
+			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		}
+	break;
 
-	// default:
-	// 	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-	// 	echo 'The application environment is not set correctly.';
-	// 	exit(1); // EXIT_ERROR
+	default:
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
 }
 
 /*
