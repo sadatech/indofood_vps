7c0f4f4350061d8fe6f56869ea971c7f057ce7cf
sadatech <sadatech@github.com>
2016-11-09 14:23:00 +0700

    upd ini

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -126,7 +126,10 @@ if ( ! is_php('5.4'))
 	}
 }
 
-
+if (function_exists("set_time_limit") == TRUE AND @ini_get("safe_mode") == 0)
+{
+    @set_time_limit(300);
+}
 /*
  * ------------------------------------------------------
  *  Define a custom error handler so we can log PHP errors
