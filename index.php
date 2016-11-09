c40c5252376fa3bf7315346d65cc7cac470ca24f
sadatech <sadatech@github.com>
2016-11-09 08:52:23 +0700

    upd index

--- index.php
+++ index.php
@@ -72,10 +72,11 @@ if (defined('ENVIRONMENT')) {
     switch (ENVIRONMENT) {
         case 'development':
             error_reporting(E_ALL);
+            ini_set('display_errors', 1);
             break;
         case 'testing':
         case 'production':
-            error_reporting(0);
+            error_reporting(E_ALL ^ E_NOTICE);
             ini_set('display_errors', 0);  
             break;
         default:
